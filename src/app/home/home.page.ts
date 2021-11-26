03f72f160c2bf94e9b018e41e558b3d44e3eb641
kong yuan shing <yuanshing.kong@gmail.com>
2021-11-26 20:55:39 +0800

    Update facial detection main page and Integrated camera

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(public photoService: PhotoService) { }
+
+  ngOnInit() {
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+}
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  public photo: Photo;
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photo = {
+      filepath: 'soon...',
+      webviewPath: capturedPhoto.webPath
+    };
+  }
+}
+
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
