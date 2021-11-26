1c6e236a61b73ed021ece7f4153a0931be180890
kong yuan shing <yuanshing.kong@gmail.com>
2021-11-26 23:29:25 +0800

    get base64 data of the image

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 
@@ -8,9 +8,10 @@ import { Storage } from '@capacitor/storage';
 })
 export class PhotoService {
 
-  public photo: Photo;
+  public photo: InMemoryPhoto;
 
-  constructor() { }
+  constructor() {
+  }
 
   public async addNewToGallery() {
     // Take a photo
@@ -20,14 +21,36 @@ export class PhotoService {
       quality: 100
     });
 
+    const base64Data = await this.readAsBase64(capturedPhoto);
+    console.log(base64Data);
     this.photo = {
-      filepath: 'soon...',
-      webviewPath: capturedPhoto.webPath
+      webviewPath: capturedPhoto.webPath,
+      base64Data
     };
+  }
+
+  private async readAsBase64(cameraPhoto: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath);
+    const blob = await response.blob();
+
+    const base64FullString = await this.convertBlobToBase64(blob) as string;
+    return base64FullString.split('base64,')[1];
+  }
+
+  private convertBlobToBase64(blob: Blob) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+          resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
   }
 }
 
-export interface Photo {
-  filepath: string;
+export interface InMemoryPhoto {
   webviewPath: string;
+  base64Data: string;
 }
